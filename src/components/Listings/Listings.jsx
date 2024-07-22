9911a99f76ada77a4d100fccd9c5fe4d2afd81fa
jensen-dong <jensendong141@gmail.com>
2024-07-21 23:18:17 -0700

    feat: commit current changes

--- src/components/Listings/Listings.jsx
+++ src/components/Listings/Listings.jsx
@@ -1,61 +1,78 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import './listingCard.css'
-
+import "./listingCard.css";
 
 const Listings = ({ listings }) => {
-    const [randomImages, setRandomImages] = useState({})
+  const [randomImages, setRandomImages] = useState({});
 
-    useEffect(() => {
-        const fetchRandomImages = async () => {
-            const images = {}
-            for (const listing of listings) {
-                const response = await fetch('https:picsum.photos/200')
-                images[listing._id] = response.url
-            }
-            setRandomImages(images)
-        }
-        fetchRandomImages()
-    }, [listings])
-    
-    const formatDateRange = (dates) => {
-        if (dates.length === 2) {
-            const [start, end] = dates.map(date => new Date(date).toISOString().split('T')[0]);
-            return `${start} to ${end}`;
-        }
-        return dates.map(date => new Date(date).toISOString().split('T')[0]).join(", ");
+  useEffect(() => {
+    const fetchRandomImages = async () => {
+      const images = {};
+      for (const listing of listings) {
+        const response = await fetch("https:picsum.photos/200");
+        images[listing._id] = response.url;
+      }
+      setRandomImages(images);
     };
-    
-    return (
-        <main>
-            <h1>Listings</h1>
-            <div className="grid-container">
-                {listings.length ? (
-                    listings.map((listing) => (
-                        <div key={listing._id} className="card">
-                            {randomImages[listing._id] ? (
-                                <img
-                                    src={randomImages[listing._id]}
-                                    alt={listing.title}
-                                />
-                            ) : (
-                                <div style={{ width: '200px', height: '200px', backgroundColor: '#f0f0f0' }}></div>
-                            )}
-                            <h2>{listing.title}</h2>
-                            <p>{listing.description}</p>
-                            <p className="price">Price: ${listing.price}</p>
-                            <p className="location">Location: {listing.location.city}, {listing.location.state}, {listing.location.country}</p>
-                            <p>Available Dates: {formatDateRange(listing.available_dates)}</p>
-                            <p>{listing.isBooked ? "Currently Booked" : "Available for Booking"}</p>
-                            <Link className="button" to={`/listings/${listing._id}`}>View Details</Link>
-                        </div>
-                    ))
-                ) : (
-                    <p>No listings available.</p>
-                )}
+    fetchRandomImages();
+  }, [listings]);
+
+  const formatDateRange = (dates) => {
+    if (dates.length === 2) {
+      const [start, end] = dates.map(
+        (date) => new Date(date).toISOString().split("T")[0]
+      );
+      return `${start} to ${end}`;
+    }
+    return dates
+      .map((date) => new Date(date).toISOString().split("T")[0])
+      .join(", ");
+  };
+
+  return (
+    <main>
+      <h1>Listings</h1>
+      <div className="grid-container">
+        {listings.length ? (
+          listings.map((listing) => (
+            <div key={listing._id} className="card">
+              {randomImages[listing._id] ? (
+                <img src={randomImages[listing._id]} alt={listing.title} />
+              ) : (
+                <div
+                  style={{
+                    width: "200px",
+                    height: "200px",
+                    backgroundColor: "#f0f0f0",
+                  }}
+                ></div>
+              )}
+              <h2>{listing.title}</h2>
+              <p>{listing.description}</p>
+              <p className="price">Price: ${listing.price}</p>
+              <p className="location">
+                Location: {listing.location.city}, {listing.location.state},{" "}
+                {listing.location.country}
+              </p>
+              <p>Available Dates: {formatDateRange(listing.available_dates)}</p>
+              <p>
+                {listing.isBooked
+                  ? "Currently Booked"
+                  : "Available for Booking"}
+              </p>
+              <Link className="button" to={`/listings/${listing._id}`}>
+                View Details
+              </Link>
+              <Link className="button" to={`/reviews/new/${listing._id}`}> Add Review</Link>
+              <Link className="button" to={`/reviews/find/${listing._id}`}> View Reviews</Link>
             </div>
-        </main>
-    );
+          ))
+        ) : (
+          <p>No listings available.</p>
+        )}
+      </div>
+    </main>
+  );
 };
 
 export default Listings;
